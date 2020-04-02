7f85038156e3ee35c5b0ae862197b5f51248eb8b
puckish-system-8282 <bryce.boddy@gmail.com>
2020-04-02 05:51:18 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,8 +1,8 @@
 def oxford_comma(array)
   if array.length == 1
-    return "#{array[0]} and #{array[1]}"
+    return array
   elsif array.length == 2
-    
+    "#{array[0]} and #{array[1]}"
   elsif array.length > 2
     
   end
